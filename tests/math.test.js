3887d71c788b167b5563f2f50aad0b7c20b6dc38
jhoset10 <jhosetceron@aol.com>
2019-12-02 14:57:40 -0500

    Added Jest framework to write test cases and wrote some cases to make sure it works

---
+++ tests/math.test.js
@@ -0,0 +1,14 @@
+// First test case.
+// Takes two parameters,
+// the string for your test case and
+// function that verifies that a given feature is
+// working as specting.
+test('Hello world!', () => {
+
+});
+
+test('This should fail', () => {
+
+  throw new Error('Failure')
+
+});
