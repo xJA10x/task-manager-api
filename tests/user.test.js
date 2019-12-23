d75162df3d7fa687e9e68314a598794065f80146
jhoset10 <jhosetceron@aol.com>
2019-12-22 20:41:41 -0500

    Wrote some test cases using jest

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,10 +1,39 @@
+///////////////////////////////////////
+//
 // File to test task manager application.
+//
+///////////////////////////////////////
 
 // Imports supertest npm library.
 const request = require('supertest');
-
 // Initializes express app.
 const app = ('../scr/app');
+// Impors User model.
+const User = require('../src/models/user');
+
+// Using jest for testing.
+
+// Creates new user.
+const userOne = {
+
+  name: 'Mike',
+  email: 'mike@example.com',
+  password: '56what!!'
+
+}
+
+// Function call
+// using async await.
+// The purpose of this function is to wipe all the data from the database.
+beforeEach(async () => {
+
+  // Deletes all the users from the database.
+  await User.deleteMany()
+
+  // Saves user to the database.
+  await new User(userOne).save()
+
+});
 
 // Function call.
 // Takes one parameter,
@@ -40,5 +69,32 @@ test('Should signup a new user', async () => {
     password: 'MyPass777!'
 
   }).expect(201)
+
+});
+
+
+// Test case.
+test('should login existing user', async () => {
+
+  // Endpoint for posting.
+  await request(app).post('/users/login').send({
+
+    email: userOne.email,
+    password: userOne.password
+
+  }).expect(200)
+
+});
+
+// Test case.
+test('Should not login nonexistent user', async() => {
+
+  // Endpoint for posting.
+  await request(app).post('/users/login').send({
+
+    emai: userOne.email,
+    password: 'thisisnotmypass'
+
+  }).expect(400)
 
 });
