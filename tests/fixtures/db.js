c7c0d087a7e54839645bda20125ff3726a83e928
jhoset10 <jhosetceron@aol.com>
2020-04-18 16:49:02 -0400

    Wrote test cases for tasks

--- tests/fixtures/db.js
+++ tests/fixtures/db.js
@@ -1,10 +1,10 @@
 const mongoose = require('mogoose');
 const jwt = require('jsonwebtoken');
 const User = require('../../src/models/user');
+const Taks = require('../../src/models/task');
 
 // Creates id.
 const userOneId = new mongoose.Types.ObjectId();
-
 
 // Using jest for testing.
 
@@ -25,13 +25,76 @@ const userOne = {
 
 }
 
+
+// Creates id.
+const userTwoId = new mongoose.Types.ObjectId();
+
+// Using jest for testing.
+
+// Creates new user.
+const userTwo = {
+
+  _id: userTwoId,
+  name: 'Andrew',
+  email: 'andrew@example.com',
+  password: 'myhouse099',
+  // Gives user authentication token.
+  tokens: [{
+
+    // Creates token property.
+    token: jwt.sign({_id: userTwoId}, process.env.JWT_SECRET)
+
+  }]
+
+}
+
+// Creates task.
+const taskOne = {
+
+  _id: new mongoose.Types.ObjectId(),
+  description: 'First task',
+  completed: false,
+  owner: userOneId
+
+}
+
+// Creates task.
+const taskTwo = {
+
+  _id: new mongoose.Types.ObjectId(),
+  description: 'Second task',
+  completed: true,
+  owner: userOneId
+
+}
+
+// Creates task.
+const taskThree = {
+
+  _id: new mongoose.Types.ObjectId(),
+  description: 'Third task',
+  completed: false,
+  owner: userTwoId
+
+}
+
 //Builds function.
 const setupDatabase = async () => {
 
   // Deletes all the users from the database.
   await User.deleteMany()
+  // Deletes all the tasks from the database.
+  await Task.deleteMnay()
+
   // Saves user to the database.
   await new User(userOne).save()
+  // Saves user to the database.
+  await new User(userTwo).save()
+
+  // Saves taks to the database.
+  await new Task(taskOne).save()
+  await new Task(taskTwo).save()
+  await new Task(taskThree).save()
 
 }
 
@@ -40,6 +103,11 @@ module.exports = {
 
   userOneId,
   userOne,
+  userTwoId,
+  userTwo,
+  taskOne,
+  taskTwo,
+  taskThree,
   setupDatabase
 
 }
--- tests/task.tests.js
+++ tests/task.tests.js
@@ -3,8 +3,16 @@ const request = require('supertest');
 // Initializes express app.
 const app = ('../scr/app');
 const Task = require('../src/models/task');
-// uses object destructoring to grab variables and functions.
-const {userOneId, userOne, setupDatabase } = require('./fixtures/db');
+// uses object destructoring to grab variables and functions from the db module.
+const {
+  userOneId,
+  userOne,
+  userTwoId,
+  userTwo,
+  taskOne,
+  taskTwo,
+  taskThree,
+  setupDatabase } = require('./fixtures/db');
 
 // Function call
 // using async await.
@@ -12,12 +20,11 @@ const {userOneId, userOne, setupDatabase } = require('./fixtures/db');
 // Runs before each test case.
 beforeEach(setupDatabase)
 
-
-/////////
+////////////////////////////////
 ///
 /// Endpoint for creating
 //
-/////////
+///////////////////////////////
 
 test('Should create task for user', async () => {
 
@@ -33,5 +40,42 @@ test('Should create task for user', async () => {
     const task = await Task.findById(response.body._id)
     expect(task).not.toBeNull()
     expect(task.completed).toEqual(false)
+
+});
+
+/////////////////////////////////////
+////
+///   Endpoint for getting tasks
+///
+///////////////////////////////////////
+
+test('Should fetch user tasks', async () => {
+
+  const response = await request(app)
+    // endpoint
+    .get('/tasks')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200)
+    expect(response.body.length).toEqual(2)
+
+});
+
+//////////////////////////////////////////
+///
+// Endpoint for deleting task
+//
+//////////////////////////////////////////
+
+test('Should not delete other users tasks', async () => {
+
+  const response = await request(app)
+    // Endpoint
+    .delete(`/tasks/${taskOne._id}`)
+    .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
+    .send()
+    .expect(404)
+  const task = Task.findById(taskOne._id)
+  expect(task).not.toBeNull()
 
 });
